77af34415b3c2b2120b2700cc18a4468eaed0ef0
Daniel Norment <danielnorment@gmail.com>
2020-03-02 12:03:20 -0800

    Calculator UI base

---
+++ calculator/calc.py
@@ -0,0 +1,8 @@
+from tkinter import Tk
+
+if __name__ == "__main__":
+    root = Tk()
+    root.configure(background="white")
+    root.title("CS 4800 Calculator")
+    root.geometry("400x600")
+    root.mainloop()
